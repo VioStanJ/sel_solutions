e06efa2f5ed445aa342779d913c13b7517aac98f
vsguerrier <vsguerrier@dinasa.ht>
2022-09-20 16:42:53 -0400

    add worker models

---
+++ database/migrations/2022_09_20_203726_create_workers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('code')->unique();
+            $table->string('type');
+            $table->text('bio');
+            $table->string('photo');
+            $table->boolean('status')->default(false);
+            $table->boolean('blocked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workers');
+    }
+}

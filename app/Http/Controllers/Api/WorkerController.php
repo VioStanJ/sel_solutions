813032e44f2d25d1bba37853e09893f4be20174b
vsguerrier <vsguerrier@dinasa.ht>
2022-12-07 13:47:56 -0500

    fix review : rm user_id

--- app/Http/Controllers/Api/WorkerController.php
+++ app/Http/Controllers/Api/WorkerController.php
@@ -12,14 +12,13 @@ class WorkerController extends Controller
     public function addReview(Request $request)
     {
         $request->validate([
-            'user_id'=>'required|exists:users,id',
             'worker_id'=>'required|exists:users,id',
             'point'=>'required',
             'note'=>'required'
         ]);
 
         WorkerReview::create([
-            'user_id'=>$request->user_id,
+            'user_id'=>$request->user()->id,
             'worker_id'=>$request->worker_id,
             'point'=>$request->point,
             'note'=>$request->note,
